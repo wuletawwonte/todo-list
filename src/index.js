64ab05764a8eda1ed0eaf18d22a6369a0324c9a4
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-24 17:42:26 +0300

    Load sorted tasks based on index of a task

--- src/index.js
+++ src/index.js
@@ -4,24 +4,26 @@ let tasks = [
   {
     description: "Go to AMU and sign the petition",
     completed: false,
-    index: 0
+    index: 3
   },
   {
     description: "Build the website for research directorate",
     completed: false,
-    index: 0
+    index: 1
   },
   {
     description: "Install linux server in the machine",
     completed: false,
-    index: 0
+    index: 2
   }
 ];
 
 const tasksContainer = document.getElementById('tasks');
 
 const loadTasks = () => {
-  tasksContainer.innerHTML += tasks.map(task => {
+  tasksContainer.innerHTML += tasks.sort((a, b) => {
+    return a.index - b.index;
+  }).map(task => {
     return `<li>
       <div class="in-list-container">
         <input type="checkbox">
