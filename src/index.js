1b6f6f9c374de5c19d1001ba4440e825e6798972
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-24 17:52:37 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import './style.css';
 
-let tasks = [
+const tasks = [
   {
-    description: "Go to AMU and sign the petition",
+    description: 'Go to AMU and sign the petition',
     completed: false,
-    index: 3
+    index: 3,
   },
   {
-    description: "Build the website for research directorate",
+    description: 'Build the website for research directorate',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
-    description: "Install linux server in the machine",
+    description: 'Install linux server in the machine',
     completed: false,
-    index: 2
-  }
+    index: 2,
+  },
 ];
 
 const tasksContainer = document.getElementById('tasks');
 
 const loadTasks = () => {
-  tasksContainer.innerHTML += tasks.sort((a, b) => {
-    return a.index - b.index;
-  }).map(task => {
-    return `<li>
+  tasksContainer.innerHTML += `${tasks.sort((a, b) => a.index - b.index).map((task) => `<li>
       <div class="in-list-container">
         <input type="checkbox">
         <p>${task.description}</p>
       </div>
-      <button class="li-btn drag-btn"><i class="fas fa-ellipsis-v"></i></button>
-    </li>`;
-  }).join('') + `<li class="clear-task-li"><button>Clear all completed</button></li>`;
-}
+      <button type="button" class="li-btn drag-btn"><i class="fas fa-ellipsis-v"></i></button>
+    </li>`).join('')}<li class="clear-task-li"><button type="button">Clear all completed</button></li>`;
+};
 
 window.onload = loadTasks();
