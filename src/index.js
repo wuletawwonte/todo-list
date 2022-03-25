8a3d3faff38f98a89c73c62dec4547afdef6a410
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-25 12:38:44 +0300

    Add content editable task and add focus background change

--- src/index.js
+++ src/index.js
@@ -11,16 +11,18 @@ const loadTasks = () => {
   if (allTasks.length !== 0) {
     tasksContainer.innerHTML = allTasks.tasks
       .sort((a, b) => a.index - b.index)
-      .map((task) => `<li>
+      .map((task) => `<li class="task-item">
             <div class="in-list-container">
               <input type="checkbox">
-              <p>${task.description}</p>
+              <p class="task-description" contenteditable="true">${task.description}</p>
             </div>
             <button type="button" class="li-btn drag-btn"><i class="fas fa-ellipsis-v"></i></button>
           </li>`)
       .join('');
   }
 };
+
+window.onload = loadTasks();
 
 addTaskForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -34,5 +36,17 @@ addTaskForm.addEventListener('submit', (event) => {
   loadTasks();
 });
 
-window.onload = loadTasks();
+const taskDescriptions = document.querySelectorAll('.task-description');
 
+console.log(taskDescriptions);
+
+taskDescriptions.forEach((taskItem) => {
+  taskItem.addEventListener('focus', (e) => {
+    taskItem.parentElement.parentElement.style = "background-color: #fffdca";    
+  });
+  taskItem.addEventListener('blur', (e) => {
+    taskItem.parentElement.parentElement.style = "background-color: #fff";
+  })
+});
+
+const taskItems = document.querySelectorAll('.task-item');
