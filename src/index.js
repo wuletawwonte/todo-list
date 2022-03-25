783e702c7dba62e76a36fdc1269af7b16037ffd1
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-25 18:10:49 +0300

    Fix stylelint error

--- src/index.js
+++ src/index.js
@@ -11,29 +11,30 @@ const loadTasks = () => {
   if (allTasks.length !== 0) {
     tasksContainer.innerHTML = allTasks.tasks
       .sort((a, b) => a.index - b.index)
-      .map((task) => `<li class="task-item">
+      .map(
+        (task) => `<li class="task-item">
             <div class="in-list-container">
               <input type="checkbox">
               <p class="task-description" contenteditable="true" data-tid="${task.index}">${task.description}</p>
             </div>
             <button type="button" data-taskid="${task.index}" id="delete${task.index}" class="li-btn delete-btn"><i class="fa-solid fa-trash-can"></i></button>
             <button type="button" id="drag${task.index}" class="li-btn drag-btn"><i class="fas fa-ellipsis-v"></i></button>
-          </li>`)
+          </li>`,
+      )
       .join('');
-    }
 
+    const deleteBtns = document.querySelectorAll('.delete-btn');
+
+    deleteBtns.forEach((dBtn) => {
+      dBtn.addEventListener('click', () => {
+        allTasks.remove(dBtn.dataset.taskid);
+        loadTasks();
+      });
+    });
+  }
 };
 
 window.onload = loadTasks();
-
-const deleteBtns = document.querySelectorAll('.delete-btn');
-
-deleteBtns.forEach(dBtn => {
-  dBtn.addEventListener('click', (e) => {
-    allTasks.remove(dBtn.dataset.taskid);
-    loadTasks();
-  });  
-});
 
 addTaskForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -43,22 +44,20 @@ addTaskForm.addEventListener('submit', (event) => {
     index: allTasks.size() + 1,
   };
   allTasks.add(task);
-  newTask.value = null;
+  newTask.value = '';
   loadTasks();
 });
 
 const taskDescriptions = document.querySelectorAll('.task-description');
 
 taskDescriptions.forEach((taskItem) => {
-  taskItem.addEventListener('keyup', (e) => {
+  taskItem.addEventListener('keyup', () => {
     allTasks.update(taskItem.dataset.tid, taskItem.textContent);
   });
-  taskItem.addEventListener('focus', (e) => {
-    taskItem.parentElement.parentElement.style = "background-color: #fffdca";
+  taskItem.addEventListener('focus', () => {
+    taskItem.parentElement.parentElement.style = 'background-color: #fffdca';
   });
-  taskItem.addEventListener('blur', (e) => {
-    taskItem.parentElement.parentElement.style = "background-color: #fff";
-  })
+  taskItem.addEventListener('blur', () => {
+    taskItem.parentElement.parentElement.style = 'background-color: #fff';
+  });
 });
-
-
